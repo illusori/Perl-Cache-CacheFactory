acbe4716da480e6c3ab8c2c05a6bef5b101a9018
Sam Graham <git@illusori.co.uk>
2008-09-15 13:28:56 +0000

    Bumped version to 1.07_05.

--- src/lib/Cache/CacheFactory.pm
+++ src/lib/Cache/CacheFactory.pm
@@ -2,7 +2,7 @@
 # Purpose : Generic Cache Factory with various policy factories.
 # Author  : Sam Graham
 # Created : 23 Jun 2008
-# CVS     : $Id: CacheFactory.pm,v 1.13 2008-09-09 17:45:16 illusori Exp $
+# CVS     : $Id: CacheFactory.pm,v 1.14 2008-09-15 13:28:56 illusori Exp $
 ###############################################################################
 
 package Cache::CacheFactory;
@@ -20,7 +20,7 @@ use Cache::CacheFactory::Object;
 
 use base qw/Cache::Cache/;
 
-$Cache::CacheFactory::VERSION = '1.07_04';
+$Cache::CacheFactory::VERSION = '1.07_05';
 
 $Cache::CacheFactory::NO_MAX_SIZE = -1;
 
